8b62f1b2ee01a85d2ea881c069f5b4fb5e85a881
jaydeep-sigzen <jaydeep@sigzen.com>
2023-04-20 20:35:55 +0530

    Update css file name theme to desk specific theme

--- gymmgt/hooks.py
+++ gymmgt/hooks.py
@@ -11,7 +11,7 @@ app_license = "MIT"
 # ------------------
 
 # include js, css files in header of desk.html
-app_include_css = "/assets/gymmgt/css/gymmgt_theme.css"
+app_include_css = "/assets/gymmgt/css/gymmgt_desk.css"
 # app_include_js = "/assets/gymmgt/js/gymmgt.js"
 
 required_apps = ["erpnext"]
