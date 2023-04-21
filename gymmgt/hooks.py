c505770ee58c2332b895f42cf446671379d0fedc
jaydeep-sigzen <jaydeep@sigzen.com>
2023-04-21 17:17:44 +0530

    Workout plan row and work out details plan 1> Update Hooks 2> Bootstrap Css added in row and detail template html 3> Custom CSS added

--- gymmgt/hooks.py
+++ gymmgt/hooks.py
@@ -17,7 +17,7 @@ app_include_css = "/assets/gymmgt/css/gymmgt_desk.css"
 required_apps = ["erpnext"]
 
 # include js, css files in header of web template
-# web_include_css = "/assets/gymmgt/css/gymmgt.css"
+web_include_css = "/assets/gymmgt/css/gymmgt_web.css"
 # web_include_js = "/assets/gymmgt/js/gymmgt.js"
 
 # include custom scss in every website theme (without file extension ".scss")
---
+++ gymmgt/www/404.py
@@ -0,0 +1,2 @@
+def get_context(context):
+    context.http_status_code = 404
