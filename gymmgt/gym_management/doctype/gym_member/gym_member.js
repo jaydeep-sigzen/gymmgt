eef84c439727ddeed25fa541b0f880446effb48e
jaydeep-sigzen <jaydeep@sigzen.com>
2023-04-21 21:49:34 +0530

    Update doctype with some basic set up like default values. Also Added index html file

--- gymmgt/gym_management/doctype/gym_member/gym_member.js
+++ gymmgt/gym_management/doctype/gym_member/gym_member.js
@@ -2,7 +2,24 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Gym Member', {
-	// refresh: function(frm) {
 
-	// }
+	setup(frm) {
+		// write setup code
+		console.log("Form=", frm)
+		console.log(new Date());
+
+		if (frm.is_new()) {
+
+			frm.add_custom_button('Click me', () => console.log('Clicked custom button'))
+
+			frm.set_value({
+				// member_name: "TEST",
+				// joining_date: "12-04-2023"
+			})
+			frm.refresh_field('member_name');
+
+		}
+	}
+
+
 });
