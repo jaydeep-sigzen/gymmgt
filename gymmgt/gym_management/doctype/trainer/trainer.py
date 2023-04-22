a6369030cf1f4f9a1e580633f1095789e080546d
jaydeep-sigzen <jaydeep@sigzen.com>
2023-04-22 19:29:56 +0530

    Changes 1> Remove unused theme for desk and web 2> Update fixtures 3> Trainer doctype added

---
+++ gymmgt/gym_management/doctype/trainer/trainer.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, Jaydeep-Sigzen and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.website.website_generator import WebsiteGenerator
+
+class Trainer(WebsiteGenerator):
+	pass
