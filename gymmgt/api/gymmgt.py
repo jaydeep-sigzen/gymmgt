4583b200d7e03ea448bd67f0a5585381c4287bdf
jaydeep-sigzen <jaydeep@sigzen.com>
2023-04-29 01:52:23 +0530

    1> Sales Invoice 2> Permission Update for the Gym Admin 3> Custom form sales invoice 4> Make require for few doctype 5> Client Script Added for the gym membership 6>Update Gym Settings

--- gymmgt/api/gymmgt.py
+++ gymmgt/api/gymmgt.py
@@ -1,5 +1,10 @@
 import frappe
 from datetime import datetime, date
+from frappe.utils.data import add_days, today
+
+# mid core game
+# hiberite casual game'
+# WEb gls
 
 
 @frappe.whitelist()
@@ -24,8 +29,44 @@ def check_user(user_name, email_id):
 def get_default_plan():
     days = frappe.db.get_single_value('Gym Settings', 'default_days')
     amount = frappe.db.get_single_value('Gym Settings', 'registration_fee')
-    obj = {'default_days': days, 'default_amount': amount}
+    obj = {
+        'default_days': days,
+        'default_amount': amount,
+        'end_date': add_days(today(), days)
+    }
     return obj
+
+
+@frappe.whitelist()
+def update_gym_membership(duration, activation_date):
+    days = frappe.db.get_single_value('Gym Settings', 'default_days')
+    amount = frappe.db.get_single_value('Gym Settings', 'registration_fee')
+    if duration == 'Quarterly':
+        days = days * 4
+        amount = amount * 4
+    elif duration == 'Yearly':
+        days = days * 12
+        amount = amount * 12
+
+    obj = {
+        'default_days': days,
+        'default_amount': amount,
+        'end_date': add_days(activation_date, days)
+    }
+    return obj
+
+
+@frappe.whitelist()
+def update_gym_membership_end_date(activation_date):
+    days = frappe.db.get_single_value('Gym Settings', 'default_days')
+    obj = {'end_date': add_days(activation_date, days)}
+    return obj
+
+
+@frappe.whitelist()
+def get_default_diet_plan_days(from_date):
+    days = frappe.db.get_single_value('Gym Settings', 'default_diet_plan_days')
+    return add_days(from_date, days)
 
 
 @frappe.whitelist()
@@ -46,3 +87,35 @@ def create_user(customer):
     else:
         frappe.msgprint('Customer not exist Error')
         return False
+
+
+@frappe.whitelist()
+def create_sales_invoice(member, member_id, plan_name, amount):
+    print(f'\n\n\n { member }\n\n\n')
+    print(f'\n\n\n { member_id }\n\n\n')
+    print(f'\n\n\n { plan_name }\n\n\n')
+
+    print(f'\n\n\n { amount }\n\n\n')
+
+    Item_name = frappe.db.get_value("Workout Plan", plan_name, 'item')
+    # intermediate
+
+    items = [{"item_code": Item_name, "qty": 1, "rate": amount}]
+
+    sales_invoice = frappe.get_doc({
+        "doctype": "Sales Invoice",
+        "customer": member,
+        "due_date": frappe.utils.nowdate(),
+        "gym_member_id": member_id,
+        "items": items
+    })
+
+    # frappe.throw('Error')
+
+    sales_invoice.insert()
+    sales_invoice.submit()
+    # frappe.db.commit()
+
+    return True
+
+    # print(f"\n\n\n { items } \n\n\n")
--- gymmgt/gym_management/doctype/assign_diet_plan/assign_diet_plan.js
+++ gymmgt/gym_management/doctype/assign_diet_plan/assign_diet_plan.js
@@ -5,4 +5,20 @@ frappe.ui.form.on('Assign Diet Plan', {
 	// refresh: function(frm) {
 
 	// }
+	start_date: function (frm) {
+		console.log(frm.doc.start_date);
+		frappe.call({
+			method: 'gymmgt.api.gymmgt.get_default_diet_plan_days',
+			args: {
+				"from_date": frm.doc.start_date
+			},
+			callback: function (r) {
+
+				console.log(r.message);
+				frm.set_value({
+					"end_date": r.message
+				})
+			}
+		});
+	}
 });
